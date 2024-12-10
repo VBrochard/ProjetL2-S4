4154eedb4fcceb87cbadcfc056609bf4dfcdafd0
Samy Al Faraj <samy.al-faraj@etu.umontpellier.fr>
2024-12-10 13:10:52 +0100

    Retour sur Flask

--- README.md
+++ README.md
@@ -18,5 +18,12 @@ Vincent :
 
 Intégrateur
 
-Pour lancer le serveur, écrire dans un terminal "python server.py"
+Pour lancer le serveur, écrire dans un terminal:
+    -export FLASK_APP=server
+    -export FLASK_ENV=development
+    -flask run
+    -aller à l'adresse http://127.0.0.1:5000/ 
+
+Remarque : lors de l'ouverture d'un nouveau terminal, il est important de se souvenir d'activer l'environnement virtuel et de définir les variables d'environnement FLASK_ENV et FLASK_APP.
+
 
--- server.py
+++ server.py
@@ -1,16 +1,7 @@
-import socket
+from flask import Flask
 
-HOST = "127.0.0.1"  
-PORT = 65432 
+app = Flask(__name__)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
