bbd479e8c4b22bc2ef1010deac3228fa6492cebb
Samy Al Faraj <samy.al-faraj@etu.umontpellier.fr>
2024-12-10 14:29:58 +0100

    Ajout des fonctions pour avoir le mot le plus long

--- server.py
+++ server.py
@@ -6,8 +6,21 @@ def motExiste(mot):
         else:
             return False
 
-mot = "marcreghons"
-if motExiste(mot):
-    print(f"Le mot '{mot}' appartient au dictionnaire.")
-else:
-    print(f"Le mot '{mot}' n'appartient pas au dictionnaire.")
+def genererToutesLesCombis(s):
+    result = []
+    def permuter(prefixe, remaining):
+        if prefixe:
+            result.append(prefixe)
+        for i in range(len(remaining)):
+            permuter(prefixe + remaining[i], remaining[:i] + remaining[i+1:])
+    permuter("", s)
+    return result
+
+def plusLongDansUneListe(l):
+    return max(liste_mots, key=len)        
+
+def motLePlusLong(s):
+    genererToutesLesCombis(s)
+    a = plusLongDansUneListe(result)
+    print(f"Le mot le plus long avec ces lettres est {a}")
+
